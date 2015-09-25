61ca67bf31a6e3dd20d471974570c154fb737b4f
Abtin Gramian <abtin.gramian@gmail.com>
2015-09-24 17:20:35 -0700

    added setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='table_printer',
+      version='1.0',
+      description='Python Table Printer',
+      author='Abtin Gramian',
+      author_email='abtin.gramian@gmail.com',
+      url='https://github.com/agramian/table-printer',
+      packages=['table_printer'],
+     )
