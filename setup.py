2edc8d725d2ff9614ba08bbcc15f5e247ecfd2cb
Abtin Gramian <abtin.gramian@gmail.com>
2015-09-28 16:11:01 -0700

    Update setup.py
    - removed long_description

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from distutils.core import setup
 setup(name='table_printer',
       version='0.1.2',
       description='Python Table Printer',
-      long_description=open('README.md', 'r').read(),
       author='Abtin Gramian',
       author_email='abtin.gramian@gmail.com',
       url='https://github.com/agramian/table-printer',
