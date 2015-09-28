84a1fb8e170c1029d6a4dcde8b8d443d1cc56b3d
Abtin Gramian <abtin.gramian@gmail.com>
2015-09-28 15:54:21 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from distutils.core import setup
 
 setup(name='table_printer',
-      version='0.1',
+      version='0.1.1',
       description='Python Table Printer',
+      long_description=open('README.md', 'r').read(),
       author='Abtin Gramian',
       author_email='abtin.gramian@gmail.com',
       url='https://github.com/agramian/table-printer',
       packages=['table_printer'],
-      download_url = 'https://github.com/agramian/table-printer/tarball/v0.1',
+      download_url = 'https://github.com/agramian/table-printer/tarball/v0.1.1',
       keywords = ['table', 'display', 'print', 'tabular', 'data'],
       classifiers = [],
      )
